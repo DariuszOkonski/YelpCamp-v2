d968fdf3b0760be34fa3c6ea14e3c94210985c08
Dariusz Okoński <darek200180@gmail.com>
2024-03-07 20:15:40 +0100

    add db connection

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
 
 const app = express();
 
@@ -8,6 +17,16 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+    description: 'cheap camping',
+  });
+  await camp.save();
+
+  res.send(camp);
 });
 
 app.listen(3000, () => {
