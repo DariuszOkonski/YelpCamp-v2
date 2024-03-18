92cd5ee603179f7deb8a47a163ad832cc87e32b5
Dariusz Okoński <darek200180@gmail.com>
2024-03-18 21:10:52 +0100

    update show.ejs

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,15 @@ const seedDB = async () => {
 
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit impedit adipisci quaerat ipsam quae, ab autem quos recusandae commodi repudiandae distinctio facere, corrupti totam dignissimos fuga nemo. Quasi, voluptatum voluptate?',
+      price,
     });
 
     await camp.save();
